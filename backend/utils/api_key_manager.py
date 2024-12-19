f99151af3017369aec068cca6a66e49ca7070cd9
pierre fro <pierrefromont@outlook.fr>
2024-12-19 04:18:17 +0100

    reset api conf

--- backend/utils/api_key_manager.py
+++ backend/utils/api_key_manager.py
@@ -3,26 +3,8 @@ import os
 from backend.config.constants import ENV_PATH
 
 # Charger les variables d'environnement à partir du fichier .env
-# load_dotenv(dotenv_path=ENV_PATH)
-load_dotenv()
+load_dotenv(dotenv_path=ENV_PATH)
 
-# ----------------------------------------------------------------------------------------------------------------------
-# Prod
-# ----------------------------------------------------------------------------------------------------------------------
-
-
-def get_api_key():
-    api_key = os.getenv("API_KEY")
-    print(f"Retrieved API_KEY: {api_key}")  # Debug log
-    if not api_key:
-        raise Exception("API Key not found. Please set it in the .env file.")
-    return api_key
-
-# ----------------------------------------------------------------------------------------------------------------------
-# Dev
-# ----------------------------------------------------------------------------------------------------------------------
-
-'''
 def get_api_key() -> str:
     """
     Reads the TMDb API key from the .env file.
@@ -34,4 +16,3 @@ def get_api_key() -> str:
     if not api_key:
         raise Exception("API Key not found. Please set it in the .env file.")
     return api_key
-'''
